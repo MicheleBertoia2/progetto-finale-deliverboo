32437ddd9ba1c4363f27bc635ff87f5cb5cca32c
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-07-12 18:23:53 +0200

    created Migration-Model-Seeder of Restaurants

---
+++ database/migrations/2023_07_12_162024_create_restaurant_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
+            $table->char('vat_number', 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_restaurants');
+    }
+};
