0ea60c7ae296a0ace0d8c0d05783e9fac14a39dc
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-07-12 16:50:25 +0200

    created migration for  dishes table and added foreign key of  user_id

---
+++ database/migrations/2023_07_12_144202_create_dishes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug')->nullable()->unique();
+            $table->string('image_path');
+            $table->decimal('price', 8, 2);
+            $table->text('ingredients');
+            $table->int('vote');
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
