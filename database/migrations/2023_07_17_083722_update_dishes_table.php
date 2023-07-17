11f5061bbd79878f802b1bbb64aa3f208fd0ea28
GiacomoArena <giacomoarena28@gmail.com>
2023-07-17 11:05:08 +0200

    update migration with is_visible

---
+++ database/migrations/2023_07_17_083722_update_dishes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->boolean('is_visible')->default(true)->after('description');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->dropColumn('is_visible');
+        });
+    }
+};
