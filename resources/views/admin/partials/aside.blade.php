ea47f51b07052854e8911220cc3d07f59e859795
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-07-19 12:35:32 +0200

    [add] aside bar

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,10 @@
+<aside class="bg-danger text-white h-100">
+
+    <nav>
+        <ul>
+            <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a></li>
+
+        </ul>
+    </nav>
+
+    </aside>
