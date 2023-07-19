9ef154dddaec32e719b4d850c6c61b64863b2932
GiacomoArena <giacomoarena28@gmail.com>
2023-07-19 10:12:52 +0200

    btn back to home from show restaurant page

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -7,7 +7,7 @@
 
     {{-- @dump($restaurant); --}}
     <h1 class="py-4">{{ $restaurant->name }}
-
+        <a class="torna btn btn-primary" href="{{route('admin.home')}}">Torna alla Home</a>
 
         <a href="{{ route('admin.restaurants.edit', $restaurant) }}" class="btn btn-primary"><i class="fa-solid fa-pencil"></i></a>
         <form action="{{ route('admin.restaurants.destroy', $restaurant) }}" method="POST" class="d-inline" onsubmit="return confirm('Confermi l\'eliminazione del ristorante: {{ $restaurant->name }} ?')">
@@ -19,7 +19,7 @@
     </h1>
     <img src="{{ $restaurant->image ? asset('storage/'. $restaurant->image) : Vite::asset('resources/img/placeholder-img.png') }}" class="py-2 w-25" alt="{{ $restaurant->image == false ? "Nessuna immagine" : $restaurant->name }}">
     <h6 class="py-2"><strong class="text-decoration-underline">Id:</strong> {{ $restaurant->id }}</h6>
-    <h6 class="py-2"><strong class="text-decoration-underline">Nome:</strong> {{ $restaurant->nome }}</h6>
+    <h6 class="py-2"><strong class="text-decoration-underline">Nome:</strong> {{ $restaurant->name }}</h6>
     <h6 class="py-2"><strong class="text-decoration-underline">Indirizzo:</strong> {{ $restaurant->address }}</h6>
     <h6 class="py-2"><strong class="text-decoration-underline">Numero Partita IVA:</strong> {{ $restaurant->vat_number }}</h6>
 
