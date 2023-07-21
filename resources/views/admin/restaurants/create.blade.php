699f0757bcbac9d94aec6533bf55767c2ad89f2f
GiacomoArena <giacomoarena28@gmail.com>
2023-07-21 11:49:41 +0200

    .

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -36,13 +36,14 @@
         <input onchange="showImagePreview(event)" type="text" class="form-control" id="input-no-path-selected" name="noPathSelected"
         value="" style="opacity: 0; border: none; height: 0; width: 0;">
         <input onchange="showImagePreview(event), handleFileSelection(event)" type="file" class="form-control @error('image') is-invalid @enderror" id="file_input" name="">
-        <div class="d-flex align-items-end">
+
+        <div class="d-flex align-items-end flex-wrap">
             <div>
         {{-- <img height="300px" class="mt-3 bg-white px-5" id="prev-img" src="{{ Vite::asset('resources\img\placeholder-img.png') }}" alt=""> --}}
                 <img height="300px" class="mt-3 bg-white" id="prev-img" src="{{ Vite::asset('resources\img\placeholder-img.png') }}" alt="img">
             </div>
             {{-- * il button deve essere type="button" oppure diverrà automaticamnete type="submit" --}}
-            <button type="button" class="btn btn-danger ms-3 mt-3" id="deleteButton" onclick="deleteImage()" style="width: 180px; height: 70px;"><span class="fa-solid fa-trash"></span> Elimina immagine <input id="inputDeleteImage" type="text" value="empty" name="noImage" style="opacity: 0; border: none; height: 0; width: 0;"></button>
+            <button type="button" class="btn btn-danger ms-1 " id="deleteButton" onclick="deleteImage()" style="width: 50px; height: 50px;"><span class="fa-solid fa-trash"></span><input id="inputDeleteImage" type="text" value="empty" name="noImage" style="opacity: 0; border: none; height: 0; width: 0;"></button>
         </div>
         <div class="mb-3">
             <label for="address" class="form-label">Indirizzo</label>
