f589f00ac9bc5060ad18e5e9b0f69a923305a159
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-07-21 15:48:49 +0200

    home fixed

--- resources/js/components/Restaurant.vue
+++ resources/js/components/Restaurant.vue
@@ -12,12 +12,15 @@ export default {
 <template>
     <div class="restaurant">
         <div class="bg-restaurant">
-            <div class="badge text center">{{ restaurant.name }}</div>
+            <div class="text center">{{ restaurant.name }}</div>
         </div>
         <div class="info">
-            <p><i class="fa-solid fa-star"></i></p>
+            <div class="continer-badge">
+                <span class="badge badge-turco">Turco</span>
+                <span class="badge badge-indiano">Indiano</span>
+                <span class="badge badge-italiano">Italiano</span>
+            </div>
             <p><i class="fa-solid fa-truck"></i></p>
-            <p>tempo</p>
         </div>
     </div>
 
@@ -38,11 +41,51 @@ export default {
             justify-content: space-evenly;
             align-items: center;
             height: 30px;
+                .continer-badge{
+                    display: flex;
+                    justify-content: center;
+
+
+                    .badge{
+                        color:black;
+                        margin-left: 3px;
+                        margin-bottom: 6px;
+                    }
+
+                    .badge-turco{
+                        background-color: #efc4ff ;
+                    }
+                    .badge-giapponese{
+                        background-color: #414bb2 ;
+                    }
+                    .badge-italiano{
+                        background-color: #77e334 ;
+                    }
+                    .badge-thai{
+                        background-color: #08f6ea ;
+                    }
+                    .badge-messicano{
+                        background-color: #f5a700 ;
+                    }
+                    .badge-indiano{
+                        background-color: #dd0d1e ;
+                    }
+                    .badge-cinese{
+                        background-color: #a708f6 ;
+                    }
+                    .badge-americano{
+                        background-color: #f6088b ;
+                    }
+                }
             }
         .bg-restaurant{
             border-top-left-radius: 10%;
             border-top-right-radius: 10%;
-            background-image: url('img/prova-ristorante.png');
+            background:
+                linear-gradient(
+                rgba(0, 0, 0, 0.3),
+                rgba(0, 0, 0, 0.3)),
+                url('img/prova-ristorante.png');
             background-size: cover;
             height: 340px;
             position: relative;
@@ -54,15 +97,15 @@ export default {
                 transform: translate(-50%, -50%);
                 }
             .text{
+                display: flex;
+                justify-content: center;
+                width: 350px;
+                color: white;
                 font-size: 25px;
                 font-weight: bold;
-                color:rgb(0, 0, 0);
                 position: absolute;
             }
-            .badge{
-                border-radius: 10%;
-                background-color: rgba(255, 255, 255, 0.051);
-            }
+
         }
     }
 </style>
