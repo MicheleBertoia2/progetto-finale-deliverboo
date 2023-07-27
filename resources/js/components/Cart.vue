3a14402f2fa1a684933c7f26672afa94619911ff
MikePio <michelepiopilla@gmail.com>
2023-07-27 13:26:51 +0200

    Completed cart design in static form

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -2,37 +2,88 @@
     <transition name="modal">
       <div v-if="modalOpen" class="modal-container" @click.self="closeModal">
         <div class="mb-modal">
-          <!-- Contenuto del modal, ad esempio il carrello -->
-          <h2>Carrello</h2>
-          <div class="btn btn-danger" @click="this.store.emptyCart()">Svuota Carrello</div>
-          <ul v-if="store.cartItems.length > 0">
-            <li v-for="(item, index) in cartItems" :key="index" class="d-flex">
-                <div class="imagebox">
-                    <img :src="store.getFullImageUrl(item.image_path)" :alt="item.name">
+            <div class="d-flex justify-content-between align-items-center">
+                <div>Il tuo ordine</div>
+                <div class="d-flex align-items-center">
+                    <div>Chiudi Carrello</div>
+                    <div class="bg-primary-color ms-2 rounded close-button d-flex justify-content-center align-items-center"><i class="fa-solid fa-close"></i></div>
                 </div>
-                <div class="info">
-                    <h4>{{ item.name }}</h4>
-                    <p>{{ item.description }}</p>
-                    <div class="quantity-interface d-flex">
-                        <div class="btn btn-secondary" @click="store.modifyQuantity(item,false)"><i class="fa-solid fa-minus"></i></div>
-                        <div class="text-center px-3 py-1 border border-black">{{ item.quantity }}</div>
-                        <div class="btn btn-secondary" @click="store.modifyQuantity(item,true)"><i class="fa-solid fa-plus" ></i></div>
-                        <div class="btn btn-danger" @click="removeFromCartAndEmit(item.id),item.isAdded = false"><i class="fa-solid fa-close" ></i></div>
+            </div>
+
+            <div class="my-3">Stai acquistando da: </div>
+
+            <div class="container-items">
+
+              <div class="d-flex justify-content-start align-items-start h-item mb-3">
+                  <img class="img-product " src="/img/jumbo.jpg" alt="">
+                  <div class="d-flex flex-column justify-content-start details-product ms-2">
+                    <div>Nome Prodotto</div>
+                    <div class="text-muted mt-2">Quantità: 1</div>
+                    <div class="d-flex ">
+                      <div class="mt-1 mb-3 btn-quantity bg-primary-color d-flex justify-content-center align-items-center">-</div>
+                      <div class="mt-1 mb-3 btn-quantity bg-primary-color d-flex justify-content-center align-items-center ms-2">+</div>
                     </div>
-                </div>
+                    <div class="bg-danger rounded p-2 remove-product text-white"><i class="fa-solid fa-trash"></i> Rimuovi</div>
+                  </div>
+                  <div class="mt-2">10.00 €</div>
+              </div>
 
-            </li>
-          </ul>
-          <div v-else><p>Non ci sono elementi nel carrello!</p></div>
-          <div class="mb-btn-close" @click="closeModal"><i class="fa-solid fa-close"></i>
+              <div class="d-flex justify-content-start align-items-start h-item mb-3">
+                  <img class="img-product " src="\img\bg-60.jpg" alt="">
+                  <div class="d-flex flex-column justify-content-start details-product ms-2">
+                    <div>Nome Prodotto</div>
+                    <div class="text-muted mt-2">Quantità: 1</div>
+                    <div class="d-flex ">
+                      <div class="mt-1 mb-3 btn-quantity bg-primary-color d-flex justify-content-center align-items-center">-</div>
+                      <div class="mt-1 mb-3 btn-quantity bg-primary-color d-flex justify-content-center align-items-center ms-2">+</div>
+                    </div>
+                    <div class="bg-danger rounded p-2 remove-product text-white"><i class="fa-solid fa-trash"></i> Rimuovi</div>
+                  </div>
+                  <div class="mt-2">10.00 €</div>
+              </div>
+
             </div>
 
-            <div v-if="store.cartItems.length > 0">
-                <h5>Totale Provvisorio: {{ orderTotal.toFixed(2) }}€</h5>
-                <div  class="btn btn-success">
-                    <router-link :to="{ name: 'checkout' }" class="nav-link">Procedi con l'ordine</router-link>
-                </div>
+            <div class="actions-bar d-flex justify-content-between align-items-end mb-2">
+              <div class="tot">Totale: 100 €</div>
+              <div class="bg-primary-color p-2 rounded mx-2 btn-order">Procedi all'ordine</div>
+              <div class="bg-danger rounded p-2 text-white"><i class="fa-solid fa-trash"></i> Svota Carrello</div>
             </div>
+
+
+
+
+          <!-- Contenuto del modal, ad esempio il carrello -->
+          <!-- <h2>Carrello</h2> -->
+          <!-- <div class="btn btn-danger" @click="this.store.emptyCart()">Svuota Carrello</div> -->
+          <!-- <ul v-if="store.cartItems.length > 0"> -->
+            <!-- <li v-for="(item, index) in cartItems" :key="index" class="d-flex"> -->
+                <!-- <div class="imagebox"> -->
+                    <!-- <img :src="store.getFullImageUrl(item.image_path)" :alt="item.name"> -->
+                <!-- </div> -->
+                <!-- <div class="info"> -->
+                    <!-- <h4>{{ item.name }}</h4> -->
+                    <!-- <p>{{ item.description }}</p> -->
+                    <!-- <div class="quantity-interface d-flex"> -->
+                        <!-- <div class="btn btn-secondary" @click="store.modifyQuantity(item,false)"><i class="fa-solid fa-minus"></i></div> -->
+                        <!-- <div class="text-center px-3 py-1 border border-black">{{ item.quantity }}</div> -->
+                        <!-- <div class="btn btn-secondary" @click="store.modifyQuantity(item,true)"><i class="fa-solid fa-plus" ></i></div> -->
+                        <!-- <div class="btn btn-danger" @click="removeFromCartAndEmit(item.id),item.isAdded = false"><i class="fa-solid fa-close" ></i></div> -->
+                    <!-- </div> -->
+                <!-- </div> -->
+
+            <!-- </li> -->
+          <!-- </ul> -->
+          <!-- <div v-else><p>Non ci sono elementi nel carrello!</p></div> -->
+          <!-- <div class="mb-btn-close" @click="closeModal"><i class="fa-solid fa-close"></i> -->
+            <!-- </div> -->
+
+            <!-- <div v-if="store.cartItems.length > 0"> -->
+                <!-- <h5>Totale Provvisorio: {{ orderTotal.toFixed(2) }}€</h5> -->
+                <!-- <div  class="btn btn-success"> -->
+                    <!-- <router-link :to="{ name: 'checkout' }" class="nav-link">Procedi con l'ordine</router-link> -->
+                <!-- </div> -->
+            <!-- </div> -->
         </div>
       </div>
     </transition>
@@ -127,4 +178,57 @@
     border-radius: 50%;
     cursor: pointer;
   }
+
+  .bg-primary-color{
+    background-color: #3ABFB4;
+  }
+
+  .fa-close{
+    font-size: 1.7rem;
+  }
+
+  .close-button{
+    background-color: #3ABFB4;
+    padding: 8px 13px;
+    cursor: pointer;
+  }
+
+  .h-item{
+    height: 150px;
+  }
+
+  .img-product{
+    width: 150px;
+    height: 150px;
+    border-radius: 10px;
+    object-fit: cover;
+  }
+
+  .details-product{
+    height: 150px;
+    width: 215px;
+  }
+
+  .btn-quantity{
+    border-radius: 5px;
+    padding: 4px 12px;
+  }
+
+  .remove-product{
+    width: 50%;
+  }
+
+  .actions-bar{
+    font-size: 16px;
+  }
+
+  .tot{
+    width: 16%;
+  }
+
+  .btn-order{
+    width: 42%;
+  }
+
+
   </style>
