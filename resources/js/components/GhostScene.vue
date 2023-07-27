8da10c3f64bf098ba8a64dd5959ae28f118f9106
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-07-27 23:43:22 +0200

    [work in progress]

--- resources/js/components/GhostScene.vue
+++ resources/js/components/GhostScene.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="canvas-container">
-      <canvas ref="webgl"></canvas>
+    <div>
+        <div class="canvas-container">
+          <canvas ref="webgl" class="w-100 h-100"></canvas>
+        </div>
+        <div class="slogan-container">
+          <h1 class="slogan">Il mio Slogan</h1>
+        </div>
     </div>
   </template>
 
@@ -21,6 +26,17 @@
       camera.position.z = 5
       scene.add(camera)
 
+        //slogan
+      const sloganElement = document.querySelector('.slogan-container')
+        sloganElement.style.opacity = '0'
+        sloganElement.style.transform = 'translateY(20px)'
+
+        setTimeout(() => {
+        sloganElement.style.transition = 'opacity 1s, transform 1s'
+        sloganElement.style.opacity = '1'
+        sloganElement.style.transform = 'translateY(0)'
+        }, 1500)
+
 
 
       // Model
@@ -29,17 +45,22 @@
         '/threeObjects/ghost.gltf',
         (gltf) => {
           model = gltf.scene
+          model.position.set(-1, -3, 0)
+
+          model.scale.set(1.5, 1.5, 1.5)
           scene.add(model)
         },
         undefined,
         (error) => {
-          console.error('Error loading GLTF model:', error)
+            console.error('Error loading GLTF model:', error)
         }
-      )
+        )
 
       const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
         directionalLight.position.set(1, 2, 3) // Posiziona la luce direzionale
         scene.add(directionalLight)
+
+
 
       // Renderer
       renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
@@ -63,7 +84,15 @@
 
   <style>
     .canvas-container{
+      position: relative;
       width: 100%;
       height: 600px;
+    }
+    .slogan-container {
+    position: absolute;
+    top: 270px;
+    right: 250px;
+    color: white;
+    font-size: 24px;
     }
   </style>
