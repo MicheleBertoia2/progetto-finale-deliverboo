c3dfc0b017af7555e8ea0c710ad23fdf870e68e8
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-07-31 18:11:25 +0200

    [fix] same jumbo logo for touch and non touch sites

--- resources/js/components/GhostSceneTouch.vue
+++ resources/js/components/GhostSceneTouch.vue
@@ -4,10 +4,11 @@
           <canvas ref="webgl" class="w-100 h-100"></canvas>
         </div>
         <div class="slogan-container">
-          <img src="/img/slogan.png" alt="">
+            <img src="/img/fantasminellostomaco.png" alt="">
+
         </div>
         <div class="logo-container flicker-in-1">
-            <img src="/img/logojumbo.png" alt="">
+            <img src="img/logoscritta2.png" alt="">
         </div>
     </div>
   </template>
